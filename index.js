b60ba97f900d8529b76975903fa4b4ccd86eaadb
David Shin <davidshin444@gmail.com>
2018-12-13 17:21:32 -0800

    Make two inputs work, and get clear and red easter eggs in

--- index.js
+++ index.js
@@ -1,12 +1,36 @@
 console.log("read index.js");
 
-var $box1 = document.getElementById("box1");
-var $box2 = document.getElementById("box2");
-var $paragraph = document.getElementById("paragraph");
+const $box1 = document.getElementById("box1");
+const $box2 = document.getElementById("box2");
+const $paragraph = document.getElementById("paragraph");
 
 function doSomething(val){
-    console.log("val: " + val);
-    console.log("$box2", $box2)
-    $box2.value = val;
-    $paragraph.textContent = val;
+    theAction(true, val);
+}
+
+function doSomethingElse(val){
+    theAction(false, val);
+}
+
+function theAction(boxOne, text){
+    console.log("text: " + text);
+    console.log("$paragraph", $paragraph);
+    if (boxOne) {
+        $box2.value = text;
+        console.log("$box2", $box2);
+    } else {
+        $box1.value = text;
+        console.log("$box1", $box1);
+    }
+
+    if (text.slice(-5) == "clear") {
+        text = "";
+        $box1.value = "";
+        $box2.value = "";
+    }
+
+    $paragraph.textContent = text;
+    if ($paragraph.textContent.slice(-3) == "red") {
+        $paragraph.style.color = "red";
+    }
 };
