87bebfc442a20ba676d8e34078720ba4fabc70d9
David Shin <davidshin444@gmail.com>
2018-12-13 16:18:31 -0800

    Moved script tag to bottom

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 console.log("read index.js");
-var outputBox;
-var outputH1;
 
-window.addEventListener('load', function() {
-    outputBox = document.getElementById("output-box");
-    outputH1 = document.getElementById("output-h1");
-});
+var outputBox = document.getElementById("output-box");
+var outputH1 = document.getElementById("output-h1");
 
 function doSomething(val){
     console.log("val: " + val);
